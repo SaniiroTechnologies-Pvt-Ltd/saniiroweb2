792ca6febf5d168caa0c87df1e15d69db4eeed75
Sanjay Jangid <sanjay.jangid@saniiro.com>
2025-05-12 14:15:35 +0530

    // fix: resolve Navbar visibility issue on scroll by removing overflowX: 'hidden'

--- components/PageWrapper.jsx
+++ components/PageWrapper.jsx
@@ -2,15 +2,13 @@ import { Container } from '@mui/material';
 
 const PageWrapper = ({ children }) => (
  <Container
-  // component={"main"}
-  maxWidth={false}
+  component={"main"}
+  maxWidth={'1536px'}
   sx={{
-   // margin: { xs: "0 auto" },
-   mx: "auto",
-   boxSizing: 'border-box',
+   margin: { xs: "0 auto" },
+   boxSizing: 'content-box',
    padding: { xs: 0 },
    width: '100%',
-   overflowX: 'hidden'
   }}
  >
   {children}
