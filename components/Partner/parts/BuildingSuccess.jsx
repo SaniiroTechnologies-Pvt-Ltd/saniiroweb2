6cb717bc73c22fbd718b5068ab335abc171611b6
Sanjay Jangid <sanjay.jangid@saniiro.com>
2025-05-12 14:18:24 +0530

    fix: adjust CSS alignment for Become A Partner banner in BuildingSuccess.jsx component

--- components/Partner/parts/BuildingSuccess.jsx
+++ components/Partner/parts/BuildingSuccess.jsx
@@ -1,183 +1,136 @@
-"use client";
+"use client"
 
-import { Box, Stack, Typography } from "@mui/material";
-import Button from "@/components/undo/Button";
 import React from "react";
+import { Box, Stack, Typography, useTheme } from "@mui/material";
+import Button from "@/components/utils/Button";
 import LinkButton from "@/components/utils/LinkButton";
 
-export default function BuildingSuccess() {
+const BuildingSuccess = () => {
+  const theme = useTheme();
+
   return (
-    <React.Fragment>
-      <Stack
+    <Stack
+      sx={{
+        background:
+          "linear-gradient(180deg, rgba(133, 159, 255, 0) 38.85%, #859FFF 84.69%)",
+        width: "100%",
+        height: "content",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "Start",
+        alignItems: "center",
+        pb: { xs: 1, md: 0 },
+      }}
+    >
+      <Box
+        maxWidth={"1536px"}
+        margin={"0 auto"}
         sx={{
-          background:
-            "linear-gradient(180deg, rgba(133, 159, 255, 0) 38.85%, #859FFF 84.69%)",
-          width: "100%",
-          height: "100vh",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "Start",
-          alignItems: "center",
+          width: { xs: "95%", lg: "98%" },
         }}
       >
-        <Box
-          maxWidth={"1536px"}
-          margin={"0 auto"}
-          sx={{
-            width: { xs: "95%", lg: "98%" },
-          }}
-        >
-          <Box>
-            <Typography
-              sx={{
-                marginTop: {
-                  xs: "30px",
-                  sm: "35px",
-                  md: "50px",
-                  lg: "95px",
-                },
-                fontFamily: "Work Sans",
-                fontSize: {
-                  xs: "24px",
-                  lg: "30px",
-                },
-                fontWeight: 600,
-                lineHeight: "30px",
-                letterSpacing: "0em",
-                textAlign: "center",
-                color: "#4DB267",
-              }}
-            >
-              BUILDING SUCCESS TOGETHER
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              sx={{
-                marginTop: "15px",
-                fontFamily: "Work Sans",
-                fontSize: {
-                  xs: "24px",
-                  sm: "30px",
-                  md: "35px",
-                  lg: "50px",
-                },
-                fontWeight: 600,
-                lineHeight: "53px",
-                letterSpacing: "0em",
-                textAlign: "center",
-                color: "#052973",
-              }}
-            >
-              Saniiro Consulting Partner Program
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              margin={"0 auto"}
-              width="95%"
-              sx={{
-                marginTop: "26px",
-                fontFamily: "Work Sans",
-                fontSize: {
-                  xs: "12px",
-                  sm: "14px",
-                  md: "16px",
-                  lg: "18px",
-                },
-                fontWeight: 400,
-                lineHeight: "27px",
-                letterSpacing: "0em",
-                textAlign: "center",
-              }}
-            >
-              Our Partner Program gives you access to one of the broadest SaaS
-              product portfolios in the industry, training to build powerful Zoho
-              product capabilities, and a dedicated partner support channel to
-              help you implement successful solutions for your customers. Join us
-              in establishing a partnership built on integrity, respect, and a
-              long-term commitment to excellence.
-            </Typography>
-          </Box>
-          <Stack
-            direction={{
-              xs: "column",
-              sm: "column",
-              md: "row",
-              lg: "row",
-            }}
-            alignItems={"center"}
-            justifyContent={"center"}
+        <Box>
+          <Typography
+            variant="h4"
             sx={{
-              marginTop: "48px",
-              gap: "10px",
+              marginTop: { xs: "30px", sm: "35px", md: "50px", lg: "95px", },
+              fontSize: { xs: "24px", lg: "30px", },
+              fontWeight: theme.typography.fontWeightBold,
+              lineHeight: theme.typography.pxToRem(30),
+              letterSpacing: theme.typography.pxToRem(0),
+              color: theme.palette.tertiary.main,
+              textAlign: "center",
             }}
           >
-            <Button
-              sx={{
-                width: {
-                  xs: "130px",
-                  md: "214px",
-                },
-                height: {
-                  xs: "40px",
-                  md: "58px",
-                },
-                padding: {
-                  xs: "10px 10px",
-                  md: "25px 20px",
-                  lg: "25px 20px",
-                },
-                fontSize: {
-                  xs: "12px",
-                  md: "16px",
-                  lg: "18px",
-                },
-                backgroundColor: "#F15B25",
-                color: "white",
-                "&:hover": {
-                  backgroundColor: "white",
-                  color: "black",
-                  border: "1px solid black",
-                },
-              }}
-            >
-              Become a Partner
-            </Button>
-            <LinkButton
-              sx={{
-                width: {
-                  xs: "120px",
-                  md: "214px",
-                },
-                height: {
-                  xs: "30px",
-                  md: "58px",
-                },
-                padding: {
-                  xs: "5px 5px",
-                  md: "0px 0px",
-                  lg: "0px 0px",
-                },
-                fontSize: {
-                  xs: "12px",
-                  md: "16px",
-                  lg: "18px",
-                },
-                border: "1px solid #000000",
-                color: "black",
-                backgroundColor: "white",
-                "&:hover": {
-                  backgroundColor: "#F15B25",
-                  color: "white",
-                  border: "1px solid #F15B25",
-                },
-              }}
-              href="/Resources/CaseStudy"> Case Studies</LinkButton>
-
-          </Stack>
+            BUILDING SUCCESS TOGETHER
+          </Typography>
         </Box>
-      </Stack>
-    </React.Fragment>
+        <Box>
+          <Typography
+            variant="h3"
+            sx={{
+              marginTop: theme.typography.pxToRem(15),
+              fontSize: { xs: "24px", sm: "30px", md: "35px", lg: "50px", },
+              fontWeight: theme.typography.fontWeightBold,
+              lineHeight: { sx: theme.typography.pxToRem(2), md: theme.typography.pxToRem(53) },
+              letterSpacing: theme.typography.pxToRem(0),
+              color: theme.palette.primary.main,
+              textAlign: "center",
+            }}
+          >
+            Saniiro Consulting Partner Program
+          </Typography>
+        </Box>
+        <Box>
+          <Typography
+            variant="subtitle1"
+            margin={"0 auto"}
+            width="95%"
+            sx={{
+              marginTop: theme.typography.pxToRem(26),
+              fontSize: { xs: "12px", sm: "14px", md: "16px", lg: "18px", },
+              fontWeight: theme.typography.fontWeightRegular,
+              lineHeight: theme.typography.pxToRem(27),
+              letterSpacing: theme.typography.pxToRem(0),
+              textAlign: "center",
+            }}
+          >
+            Our Partner Program gives you access to one of the broadest SaaS
+            product portfolios in the industry, training to build powerful Zoho
+            product capabilities, and a dedicated partner support channel to
+            help you implement successful solutions for your customers. Join us
+            in establishing a partnership built on integrity, respect, and a
+            long-term commitment to excellence.
+          </Typography>
+        </Box>
+        <Stack
+          direction={{ xs: "column", md: "row", }}
+          alignItems={"center"}
+          justifyContent={"center"}
+          sx={{
+            marginTop: theme.typography.pxToRem(48),
+            gap: theme.typography.pxToRem(10),
+          }}
+        >
+          <Button
+            sx={{
+              width: { xs: "130px", md: "214px", },
+              height: { xs: "40px", md: "58px", },
+              padding: { xs: "10px 10px", md: "25px 20px", lg: "25px 20px", },
+              fontSize: { xs: "12px", md: "16px", lg: "18px", },
+              backgroundColor: theme.palette.secondary.main,
+              color: theme.palette.secondary.contrastText,
+              "&:hover": {
+                backgroundColor: theme.palette.common.white,
+                color: theme.palette.common.black,
+                border: `1px solid ${theme.palette.common.black}`,
+              },
+            }}
+          >
+            Become a Partner
+          </Button>
+          <LinkButton
+            sx={{
+              width: { xs: "120px", md: "214px", },
+              height: { xs: "30px", md: "58px", },
+              padding: { xs: "5px 5px", md: "0px 0px", lg: "0px 0px", },
+              fontSize: { xs: "12px", md: "16px", lg: "18px", },
+              border: `1px solid ${theme.palette.common.black}`,
+              color: theme.palette.common.black,
+              backgroundColor: theme.palette.common.white,
+              "&:hover": {
+                backgroundColor: theme.palette.secondary.main,
+                color: theme.palette.secondary.contrastText,
+                border: `1px solid ${theme.palette.secondary.main}`,
+              },
+            }}
+            href="/Resources/CaseStudy"> Case Studies</LinkButton>
+
+        </Stack>
+      </Box>
+    </Stack>
   );
 };
+
+export default BuildingSuccess;
